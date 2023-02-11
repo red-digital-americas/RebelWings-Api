b98bb14ed41d3e26aedb84f7b6ca900cc03e6556
erickGoJi <erickgomezlol96@gmail.com>
2023-02-11 10:05:45 -0600

    Add DTO Dashboard performance

--- api.rebel-wings/Models/Dashboard/DashboardAdminPerformanceDto.cs
+++ api.rebel-wings/Models/Dashboard/DashboardAdminPerformanceDto.cs
@@ -5,12 +5,16 @@ public class DashboardAdminPerformanceDto
     public DashboardAdminPerformanceDto()
     {
         Performances = new HashSet<DashboardAdminPerformanceByBranchDto>();
+        TopOmittedTask = new HashSet<TaskNoCompleteDto>();
+        Multi = new HashSet<BranchChartBarVerticalDto>();
     }
     public int Id { get; set; }
     public decimal Complete { get; set; }
     public decimal NoComplete { get; set; }
     public decimal AverageEvaluation { get; set; }
     public virtual ICollection<DashboardAdminPerformanceByBranchDto> Performances { get; set; }
+    public virtual ICollection<TaskNoCompleteDto> TopOmittedTask { get; set; }
+    public virtual ICollection<BranchChartBarVerticalDto> Multi { get; set; }
 }
 
 public class DashboardAdminPerformanceByBranchDto
@@ -20,4 +24,22 @@ public class DashboardAdminPerformanceByBranchDto
     public decimal Complete { get; set; }
     public decimal NoComplete { get; set; }
     public decimal AverageEvaluation { get; set; }
+}
+
+public class TaskNoCompleteDto
+{
+    public string Name { get; set; }
+    public int Value { get; set; }
+}
+
+public class BranchChartBarVerticalDto
+{
+    public string Name { get; set; }
+    public ICollection<SerieDto> Series { get; set; }
+}
+
+public class SerieDto
+{
+    public string Name { get; set; }
+    public decimal Value { get; set; }
 }
